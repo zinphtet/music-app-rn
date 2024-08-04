454a8597a8f6348ccd65611c5f129f4508e2e45c
zinphtet <zinpainghtet.215108@gmail.com>
2024-08-04 11:15:21 +0630

    fix err

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
+import {playbackService} from './utils/music-controller';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(()=> require('./utils/service.ts'))
+// TrackPlayer.registerPlaybackService(() => playbackService);
